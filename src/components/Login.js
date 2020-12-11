96875093f1bb93734e4d57c83093519b5c731aac
fakhira <fakhira.yaqub>
2020-12-11 15:46:51 +0100

    added typography wise comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+
 import  "./Login.css"
 import CustomInput from "./inputs/CustomInput";
 import Button from '../components/Buttons/Buttons.js'
+// import colors from './utils/colors';
+
+import {H1, primaryFont }  from '../utils/typography';
+import colors from "../utils/colors";
+
 
 
 
@@ -12,15 +17,17 @@ export default function Login() {
 
 
 
-  
+
   return<div className="login_container">
-    <h3> Enter Your Login Details</h3>
-   <CustomInput placeholder="Email" type="text" />
+    <H1>Enter Your Login Details</H1>
+   <CustomInput placeholder="Email" type="text" font-familu={primaryFont}/>
    <CustomInput placeholder="Password" type="password"/>
    <br></br>
    <br></br>
-   
-   
    <Button>Login</Button>
+   <br></br>
+   <br></br>
+   <a href="" color = {colors.orangePink} id="password-reset" >Forget Password</a>
+  
    </div>
 }
