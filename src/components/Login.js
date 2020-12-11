bfb9a6ba61f6ca10586d6f7d1bbbc85dc23dec21
fakhira <fakhira.yaqub>
2020-12-11 16:00:06 +0100

    added input and next button

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import Button from '../components/Buttons/Buttons.js'
 
 import {H1, primaryFont }  from '../utils/typography';
 import colors from "../utils/colors";
+import Typography from "typography";
 
 
 
@@ -20,14 +21,14 @@ export default function Login() {
 
   return<div className="login_container">
     <H1>Enter Your Login Details</H1>
-   <CustomInput placeholder="Email" type="text" font-familu={primaryFont}/>
+   <CustomInput placeholder="Email" type="text" />
    <CustomInput placeholder="Password" type="password"/>
    <br></br>
    <br></br>
    <Button>Login</Button>
    <br></br>
    <br></br>
-   <a href="" color = {colors.orangePink} id="password-reset" >Forget Password</a>
+   <a href="" colors={colors.primary} id="password-reset" >Forget Password</a>
   
    </div>
 }
