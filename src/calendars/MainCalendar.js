557acd6d816b4ae9e3944a5e10eab21872098a5a
Annika Davis <annikadavis@gmail.com>
2020-12-17 11:00:23 +0100

    initial calendar working

--- src/calendars/MainCalendar.js
+++ src/calendars/MainCalendar.js
@@ -10,24 +10,27 @@ export default function MainCalendar() {
   const [date, setDate] = useState();
   const [nextPeriod, setNextPeriod] = useState([]);
   const [nextOvulation, setNextOvulation] = useState([]);
+  const [input, setInput] = useState([]);
 
   const handlePickDate = (date) => {
-    let output = [add(new Date(date), { days: 28 })];
+    let output = [add(new Date(date), { days: input-1 })];
     for (let i = 0; i < 6; i++) {
       output.push(add(new Date(output[i]), { days: 1 }));
     }
     setDate(date);
     setNextPeriod(output);
+    handlePickOvulation(date);
+    console.log("output", output)
   };
 
   const handlePickOvulation = (date) => {
-    let output = [add(new Date(date), { days: 14 })];
+    let output = [add(new Date(date), { days: (input/3) })];
     for (let i = 0; i < 4; i++) {
       output.push(add(new Date(output[i]), { days: 1 }));
     }
-    // setDate(date);
     setNextOvulation(output);
   };
+
 
   const modifiers = {
     highlight: (date) => {
@@ -47,19 +50,45 @@ export default function MainCalendar() {
     ovulation: "-ovulation"
   };
 
+  console.log(nextPeriod);
+  console.log("next period 0", nextPeriod[0])
+  
+
+  // const handleFormSubmit = (event) => {
+  //   console.log(input)
+  // }
+
   return (
+    <div>
+    <div>
+      <p>How many days does your cycle usually last? The average is 28 days.</p>
+      <form>
+        <input
+          className="form"
+          type="number"
+          min="20"
+          max="40"
+          placeholder="enter number of days"
+          value={input}
+          onChange={(event) => {console.log("days", input);setInput(event.target.value)}}
+        />
+         {/* <input type="submit" disabled={!input ? true : false} value="submit" class="button" /> */}
+        </form></div>
     <div className='calendar'>
       <p>
-        Selected date:
+        Please select the first day of your last period. 
+        <br />
+        Selected date: 
         {date ? format(date, "dd MMM yyyy", { locale: enGB }) : "none"}.
       </p>
       <DatePickerCalendar
         date={date}
-        onDateChange={handlePickOvulation, handlePickDate}
+        onDateChange={handlePickDate}
         modifiers={modifiers}
         modifiersClassNames={modifiersClassNames}
         locale={enGB}
+        months={2}
       />
-    </div>
-  );
+    </div></div>
+  )
 }
