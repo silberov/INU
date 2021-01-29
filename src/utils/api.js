e4924d32a15475287d05ae47f938b5717dabe7d0
fakhira <fakhira.yaqub>
2021-01-29 10:09:35 +0100

    fixed cors prob

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 export async function postDataToPath(path, data) {
   const BASE_URL = process.env.REACT_APP_BACK_END_URL;
-  const url = `${BASE_URL}${path}`;
+  const url = `${path}`;
   const response = await fetch(url, {
     method: "POST",
     credentials:"include", // send cookies to server
