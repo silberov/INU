ade844521d71ff39e11b6d78baa9680b5eec7847
fakhira <fakhira.yaqub>
2021-01-29 15:59:41 +0100

    fixed button

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -38,7 +38,10 @@ export const Moon = styled.img`
   margin-left: 10px;
 `;
 
-export const Button = styled.button`
+export const Button = styled.button.attrs((props) => ({
+// we can define static props	
+onClick: props.runOnClick,	
+}))`  
   font-family: "tondo", sans-serif;
   font-style: normal;
   font-weight: bold;
