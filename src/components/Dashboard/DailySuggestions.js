1a4c929337ca3fc70ed631c611260ba6aa1c9d47
Inna Zilberov <70526642+silberov@users.noreply.github.com>
2021-02-17 16:52:04 +0100

    fixed dashboard

--- src/components/Dashboard/DailySuggestions.js
+++ src/components/Dashboard/DailySuggestions.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from "react";
 import { P, SubHeader } from "../typography";
 import { DashboardDiv } from "./TheraphySuggestions";
 
-import axios from "axios";
-//const API_URL = process.env.REACT_APP_API_URL;
-
-export default function DailySuggestions(props) {
-  const [quote, setQuote] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        `http://localhost:3000/api/phases/${props.phaseId}/suggestions/random`
-      )
-      .then((data) => setQuote(data));
-  }, [props]);
-
-  // console.log("data?", quote.data.text);
-
+export default function DailySuggestions({ suggestion }) {
   return (
     <DashboardDiv>
       <SubHeader modifiers={["purple"]}>Daily suggestions</SubHeader>
       <br></br>
-      <P>{quote.data && quote.data.text}</P>
+      <P>{suggestion}</P>
     </DashboardDiv>
   );
 }
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,39 +2,39 @@ import TheraphySuggestions from "./TheraphySuggestions";
 import DailySuggestions from "./DailySuggestions";
 import CurrentDate from "./CurrentDate";
 import { CalButton } from "../Buttons/Buttons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 import useCrud from "../../hooks/useCrud";
 import { differenceInCalendarDays } from "date-fns";
 import { DashboardWrap } from "../containers";
+import axios from "axios";
 
-export const phases = [
-  { id: 1, startday: 1, endday: 6, message: "Period Phase" },
-  { id: 3, startday: 7, endday: 14, message: "Follicular Phase" },
-  { id: 4, startday: 15, endday: 18, message: "Ovulation Phase" },
-  { id: 5, startday: 19, endday: 35, message: "Luteal Phase “PMS”" },
-];
-
-// DailySuggestion
 export default function Dashboard() {
   const { items, loading } = useCrud("/cycle");
+  const [suggestion, setSuggestion] = useState("");
 
   const periodStart = items.cycle?.last_period || new Date().toString();
   const startingDate = new Date(periodStart.substring(0, 10));
   const distance = differenceInCalendarDays(new Date(), startingDate) + 1; // =1
-  const filterMessage = phases
-    .filter(
-      (titlex) => titlex.startday <= distance && titlex.endday >= distance
-    )
-    .map((filteredTitle) => filteredTitle.message);
+  const currentPhase = () => {
+    if (1 <= distance && 6 >= distance) {
+      return 1;
+    } else if (7 <= distance && 14 >= distance) {
+      return 3;
+    } else if (15 <= distance && 18 >= distance) {
+      return 4;
+    } else return 5;
+  };
 
-  const filteredId = phases
-    .filter(
-      (titlex) => titlex.startday <= distance && titlex.endday >= distance
-    )
-    .map((filteredId) => filteredId.id);
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:3000/api/phases/${currentPhase()}/suggestions/random`
+      )
+      .then((data) => setSuggestion(data));
+  }, [periodStart]);
 
   return (
     <div>
@@ -45,8 +45,11 @@ export default function Dashboard() {
           <Link to="/user/calendar">
             <CalButton />
           </Link>
-          <CurrentDate counter={distance} phaseTitle={filterMessage} />
-          <DailySuggestions phaseId={filteredId} />
+          <CurrentDate
+            counter={distance}
+            phaseTitle={suggestion.data.phase.name}
+          />
+          <DailySuggestions suggestion={suggestion.data.text} />
           <TheraphySuggestions />
           <Navbar selected={"cycle"} />
         </DashboardWrap>
